2070ad34a48aacb778d20d244ee24812aaf39c74
Doombox <Aaron.j.king.95@gmail.com>
2016-08-10 22:42:41 +0100

    Cleaning up

--- QuickPaste/src/Misc/GlobalKeyboardHook.cs
+++ QuickPaste/src/Misc/GlobalKeyboardHook.cs
@@ -7,30 +7,26 @@ namespace QuickPaste
 {
     class GlobalKeyboardHook
     {
-
         private const int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
         private const int WM_SYSKEYDOWN = 0x0104;
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
         private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-
-        public event EventHandler<KeyPressedArgs> OnKeyPressed;
 
         private LowLevelKeyboardProc _proc;
         private IntPtr _hookID = IntPtr.Zero;
+
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        public event EventHandler<KeyPressedArgs> OnKeyPressed;
 
         public GlobalKeyboardHook()
         {
@@ -51,20 +47,13 @@ namespace QuickPaste
         {
             using (Process curProcess = Process.GetCurrentProcess())
             using (ProcessModule curModule = curProcess.MainModule)
-            {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
-            }
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);       
         }
 
         private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
             if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
-            {
-                int vkCode = Marshal.ReadInt32(lParam);
-
-                if (OnKeyPressed != null) { OnKeyPressed(this, new KeyPressedArgs(KeyInterop.KeyFromVirtualKey(vkCode))); }
-            }
-
+                OnKeyPressed?.Invoke(this, new KeyPressedArgs(KeyInterop.KeyFromVirtualKey(Marshal.ReadInt32(lParam))));           
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
     }
@@ -72,7 +61,6 @@ namespace QuickPaste
     public class KeyPressedArgs : EventArgs
     {
         public Key KeyPressed { get; private set; }
-
         public KeyPressedArgs(Key key)
         {
             KeyPressed = key;
